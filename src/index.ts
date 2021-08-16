305f2646ad9ef0b1901e03556a9a458edcfb41f5
Mateusz Zając <matzayonc@gmail.com>
2021-08-16 13:30:38 +0200

    creating exchange from sdk

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,35 @@
 import * as web3 from '@solana/web3.js'
 import { Connection, Keypair, PublicKey } from '@solana/web3.js'
-import { AccountsCoder } from '@project-serum/anchor'
+import { AccountsCoder, Provider, BN } from '@project-serum/anchor'
 import { Idl } from '@project-serum/anchor/dist/idl'
-import { ExchangeAccount, AssetsList, ExchangeState } from '@synthetify/sdk/lib/exchange'
+import { Network, DEV_NET } from '@synthetify/sdk/lib/network'
 import EXCHANGE_IDL from '@synthetify/sdk/src/idl/exchange.json'
+import { ExchangeAccount, AssetsList, Exchange } from '@synthetify/sdk/lib/exchange'
 
-const PROGRAM_ID = new PublicKey('2MDpnAdPjS6EJgRiVEGMFK9mgNgxYv2tvUpPCxJrmrJX')
+// const PROGRAM_ID = new PublicKey('2MDpnAdPjS6EJgRiVEGMFK9mgNgxYv2tvUpPCxJrmrJX')
 
 const coder = new AccountsCoder(EXCHANGE_IDL as Idl)
+const connection = new Connection(web3.clusterApiUrl('devnet'), 'confirmed')
+const { wallet } = Provider.local()
+const { exchange: exchangeProgram, exchangeAuthority } = DEV_NET
 
 ;(async () => {
-  console.log('Starting liquidation..')
-  var connection = new Connection(web3.clusterApiUrl('devnet'), 'confirmed')
+  console.log('Fetching state..')
 
-  var wallet = Keypair.generate()
-  await connection.requestAirdrop(wallet.publicKey, web3.LAMPORTS_PER_SOL)
+  const exchange = await Exchange.build(
+    connection,
+    Network.LOCAL,
+    wallet,
+    exchangeAuthority,
+    exchangeProgram
+  )
+  const assetsList = await exchange.getAssetsList((await exchange.getState()).assetsList)
+  console.log(assetsList)
+
+  console.log('Fetching accounts..')
 
   const accounts: ExchangeAccount[] = await (
-    await connection.getProgramAccounts(PROGRAM_ID, { filters: [{ dataSize: 509 }] })
+    await connection.getProgramAccounts(exchangeProgram, { filters: [{ dataSize: 509 }] })
   ).map(({ account }) => coder.decode<ExchangeAccount>('ExchangeAccount', account.data))
 
   console.log(accounts)
