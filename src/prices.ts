6f65eaa1a8cc9001211eac5e68801ca91a431bb6
Mateusz Zając <matzayonc@gmail.com>
2021-08-23 18:57:54 +0200

    subscription on price

--- src/prices.ts
+++ src/prices.ts
@@ -5,23 +5,9 @@ import { AssetsList } from '@synthetify/sdk/lib/exchange'
 export const prices = async (connection: Connection, assetsList: AssetsList) => {
   const asset = assetsList.assets[3]
 
-  //   const unparsed = await connection.getAccountInfo(asset.feedAddress)
-  //   const data = parsePriceData(unparsed.data)
-  //   console.log(data.price)
+  connection.onAccountChange(asset.feedAddress, (accountInfo) => {
+    const data = parsePriceData(accountInfo.data)
 
-  //   allAssets.forEach((asset, index) => {
-  //     connection.onAccountChange(asset.feedAddress, (accountInfo) => {
-  //       const data = parsePriceData(accountInfo.data)
-  //       dispatch(
-  //         actions.setAssetPrice({
-  //           tokenIndex: index,
-  //           price: { val: new BN(data.price * 10 ** asset.price.scale), scale: asset.price.scale }
-  //         })
-  //       )
-  //     })
-  //   })
-
-  const accountInfo = await connection.getAccountInfo(asset.feedAddress)
-  const data = parsePriceData(accountInfo.data)
-  console.log(data.price)
+    console.log(data.price)
+  })
 }
